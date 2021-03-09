9a149f750ea7a73a0cad5324c94cf34296e0bb08
abraaobb <abraaobritof10@gmail.com>
2021-03-09 08:53:09 -0400

    array

---
+++ 08-javascript/37-array-basico/js/meu-script.js
@@ -0,0 +1,11 @@
+var lista_frutas = new Array();
+var lista_compras = new Array('frango', 'arroz', 'farofa');
+lista_frutas[1] = 'Banana';
+lista_frutas[2] = 'Maça';
+// lista_frutas[3] = 'Morango';
+lista_frutas['x'] = 'Morango';
+lista_frutas[4] = 'Uva';
+lista_frutas[5] = 'Melancia';
+
+alert(lista_frutas['x']);
+alert(lista_compras);
